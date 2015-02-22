b6c1e7a4de084ba2c27d3e2f7582f1a9381f342b
vedina <vedina@a469c33f-3a41-0410-815f-a20f7f95d01d>
2015-02-22 19:48:32 +0000

    Descriptor specification compile error fix
    git-svn-id: svn://svn.code.sf.net/p/ambit/code/branches/ambit-3.0.0-SNAPSHOT@6366 a469c33f-3a41-0410-815f-a20f7f95d01d

--- ambit2-descriptors/src/main/java/ambit2/descriptors/constitutional/AbstractAtomCountDescriptor.java
+++ ambit2-descriptors/src/main/java/ambit2/descriptors/constitutional/AbstractAtomCountDescriptor.java
@@ -3,14 +3,17 @@ package ambit2.descriptors.constitutional;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.qsar.AbstractMolecularDescriptor;
+import org.openscience.cdk.qsar.DescriptorSpecification;
 import org.openscience.cdk.qsar.DescriptorValue;
-import org.openscience.cdk.qsar.IMolecularDescriptor;
 import org.openscience.cdk.qsar.result.DoubleResult;
 import org.openscience.cdk.qsar.result.IDescriptorResult;
 import org.openscience.cdk.qsar.result.IntegerResult;
 
-public abstract class AbstractAtomCountDescriptor implements
-		IMolecularDescriptor {
+import ambit2.base.data.Property;
+
+public abstract class AbstractAtomCountDescriptor extends
+		AbstractMolecularDescriptor {
 	protected final String[] descriptorNames;
 
 	public AbstractAtomCountDescriptor(String[] names) {
@@ -41,7 +44,16 @@ public abstract class AbstractAtomCountDescriptor implements
 	public Object getParameterType(String name) {
 		return "";
 	}
-
+	@Override
+	public DescriptorSpecification getSpecification() {
+		return new DescriptorSpecification(
+				String.format(Property.AMBIT_DESCRIPTORS_ONTOLOGY,
+						"AbstractAtomCountDescriptor"),
+				this.getClass().getName(),
+				"$Id: AbstractAtomCountDescriptor.java, v 0.1 2013 Elena Urucheva, Nikolay Kochev",
+				"http://ambit.sourceforge.net");
+	}
+	
 	@Override
 	public DescriptorValue calculate(IAtomContainer container) {
 		if (container == null) {
