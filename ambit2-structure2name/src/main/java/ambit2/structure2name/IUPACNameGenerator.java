09946300967931d6f5bfd2160fcc57f80c26f213
ntk73 <ntk73@a469c33f-3a41-0410-815f-a20f7f95d01d>
2015-03-13 13:39:07 +0000

    Added class IUPACNameGenerator.
    git-svn-id: svn://svn.code.sf.net/p/ambit/code/branches/ambit-3.0.0-SNAPSHOT@6400 a469c33f-3a41-0410-815f-a20f7f95d01d

---
+++ ambit2-structure2name/src/main/java/ambit2/structure2name/IUPACNameGenerator.java
@@ -0,0 +1,13 @@
+package ambit2.structure2name;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+public class IUPACNameGenerator 
+{
+	
+	public String generateIUPACName(IAtomContainer mol)
+	{
+		//TODO
+		return null;
+	}
+}
