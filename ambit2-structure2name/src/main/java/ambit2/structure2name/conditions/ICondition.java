e0672ef09bef77610a5d16589f406d5e49aa8668
ntk73 <ntk73@a469c33f-3a41-0410-815f-a20f7f95d01d>
2015-05-07 13:10:55 +0000

    Deleted interfaces that are to be put in ambit2-rules.
    git-svn-id: svn://svn.code.sf.net/p/ambit/code/branches/ambit-3.0.0-SNAPSHOT@6554 a469c33f-3a41-0410-815f-a20f7f95d01d

--- ambit2-structure2name/src/main/java/ambit2/structure2name/conditions/ICondition.java
+++
@@ -1,6 +0,0 @@
-package ambit2.structure2name.conditions;
-
-public interface ICondition 
-{
-	public boolean isTrue(Object target);
-}
--- ambit2-structure2name/src/main/java/ambit2/structure2name/rules/IBasicRule.java
+++
@@ -1,6 +0,0 @@
-package ambit2.structure2name.rules;
-
-public interface IBasicRule 
-{
-	
-}
