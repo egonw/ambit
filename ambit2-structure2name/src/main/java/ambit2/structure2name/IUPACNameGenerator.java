6b05dcb4886dcd5650d93652ea960c97f7903bc1
ntk73 <ntk73@a469c33f-3a41-0410-815f-a20f7f95d01d>
2015-05-15 10:49:55 +0000

    New subpackage parser. Added new class IUPACRuleDataBase
    git-svn-id: svn://svn.code.sf.net/p/ambit/code/branches/ambit-3.0.0-SNAPSHOT@6588 a469c33f-3a41-0410-815f-a20f7f95d01d

--- ambit2-structure2name/src/main/java/ambit2/structure2name/IUPACNameGenerator.java
+++ ambit2-structure2name/src/main/java/ambit2/structure2name/IUPACNameGenerator.java
@@ -3,11 +3,23 @@ package ambit2.structure2name;
 import org.openscience.cdk.interfaces.IAtomContainer;
 
 public class IUPACNameGenerator 
-{
+{	
+	protected IUPACRuleDataBase ruleDataBase = null;
 	
-	public String generateIUPACName(IAtomContainer mol)
+	public IUPACNameGenerator()
+	{
+		ruleDataBase = IUPACRuleDataBase.getDefaultRuleDataBase();
+	}
+	
+	public String generateIUPACName(IAtomContainer mol) throws Exception
 	{
 		//TODO
 		return null;
 	}
+
+	public IUPACRuleDataBase getRuleDataBase() {
+		return ruleDataBase;
+	}
+
+	
 }
