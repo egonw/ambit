cfddd2d8956eba665ef03ad2c63b495d654d8327
ntk73 <ntk73@a469c33f-3a41-0410-815f-a20f7f95d01d>
2015-05-15 11:48:56 +0000

    Changed interface.
    git-svn-id: svn://svn.code.sf.net/p/ambit/code/branches/ambit-3.0.0-SNAPSHOT@6589 a469c33f-3a41-0410-815f-a20f7f95d01d

--- ambit2-rules/src/main/java/ambit2/rules/IBasicRule.java
+++ ambit2-rules/src/main/java/ambit2/rules/IBasicRule.java
@@ -1,12 +1,10 @@
 package ambit2.rules;
 
-import java.util.List;
 
 import ambit2.rules.conditions.ICondition;
-
 
 public interface IBasicRule 
 {
 	public String getName();
-	public List<ICondition> getConditions();
+	public ICondition getCondition();
 }
