d3c52d70e2063b6247a531b5f2c0036460fec718
vedina <vedina@a469c33f-3a41-0410-815f-a20f7f95d01d>
2015-06-18 07:19:59 +0000

    splitting config-study.js
    git-svn-id: svn://svn.code.sf.net/p/ambit/code/branches/ambit-3.0.0-SNAPSHOT@6666 a469c33f-3a41-0410-815f-a20f7f95d01d

--- ambit2-apps/ambit2-www/src/main/webapp/scripts/config-study.js
+++ ambit2-apps/ambit2-www/src/main/webapp/scripts/config-study.js
@@ -1,194 +1,3 @@
-var config_toxcast = {
-	"interpretation" : {
-		"result" : {
-			"sTitle" : "hitc",
-			"bVisible" : true,
-			"iOrder" : -10,
-			"mRender" : function(data, type, full) {
-				var c = "";
-				if ("Active" == data) c = 'class="active"';
-				else if ("Inactive" == data) c = 'class="inactive"';
-				return "<span "+ c +">"+data+"</span></br/>" + full["interpretation"]["criteria"];
-			},
-			"inMatrix" : true
-		},
-		"criteria" : {
-			"bVisible" : false,
-			"iOrder" : -9
-		}
-	},
-	"protocol" : {
-		"citation" : {
-			"bVisible" : true,
-			"sTitle" : "Reference",
-			"mRender" : function(data, type, full) {
-				return "<label>"+data["owner"] + "</label><br/>" 
-				+ full["protocol"]["guideline"] + "<br/>"
-				+ data["title"];
-			},
-			"iOrder" : -1
-		},		
-		"guideline" : {
-			"bVisible" : false,
-			"iOrder" : -1		
-		},
-		"uuid" : {
-			"bVisible" : false
-		},
-		"owner" : {
-			"bVisible" : false,
-			"iOrder" : -1
-		}
-	},
-	"conditions" : {
-		"concentration" : {
-			"bVisible" : false,
-			"iOrder" : -20
-		},
-		"fitc" : {
-			"bVisible" : false,
-			"iOrder" : -20
-		},
-		"bmad" : {
-			"bVisible" : false,
-			"iOrder" : -20
-		},
-		"modl_rmse" : {
-			"bVisible" : false,
-			"sTitle" : "Model RMSE",
-			"iOrder" : -1
-		}		
-	},
-	"effects" : {
-		"endpoint" : {
-			"iOrder" : -22
-		},
-		"result" : {
-			"sTitle" : "",
-			"iOrder" : -21
-		},
-		"text" : {
-			"bVisible" : false
-		}
-	},
-	"parameters" : {
-		"detection_technology" : {
-			"iOrder" : -38,
-			"sTitle" : "Detection Technology",
-			"bVisible" : true
-		},
-		"organism" : {
-			"sTitle" : "Organism",
-			"iOrder" : -37,
-			"bVisible" : true
-		},
-		"tissue" : {
-			"sTitle" : "Tissue",
-			"iOrder" : -36,
-			"bVisible" : true
-		},
-		"cell_free_component_source" : {
-			"iOrder" : -35,
-			"bVisible" : false
-		},
-		"cell_format" : {
-			"iOrder" : -34,
-			"bVisible" : false
-		},
-		"cell_short_name" : {
-			"sTitle" : "Cell",
-			"iOrder" : -33,
-			"bVisible" : true
-		},
-		"assay_format_type" : {
-			"iOrder" : -32,
-			"bVisible" : false
-		},
-		"assay_format_type_sub" : {
-			"iOrder" : -31,
-			"bVisible" : false
-		},
-		"dilution_solvent" : {
-			"iOrder" : -30,
-			"bVisible" : false
-		},
-		"dilution_solvent_percent_max" : {
-			"iOrder" : -29,
-			"bVisible" : false
-		},
-		"assay_design_type" : {
-			"iOrder" : -28,
-			"bVisible" : false
-		},
-		"assay_design_type_sub" : {
-			"iOrder" : -27,
-			"bVisible" : false
-		},
-		"signal_direction_type" : {
-			"iOrder" : -26,
-			"bVisible" : false
-		},
-		"key_assay_reagent" : {
-			"iOrder" : -25,
-			"bVisible" : false
-		},
-		"technological_target_type" : {
-			"iOrder" : -24,
-			"bVisible" : false
-		},
-		"technological_target_type_sub" : {
-			"iOrder" : -23,
-			"bVisible" : false
-		},
-		"timepoint" : {
-			"sTitle" : "Time point",
-			"iOrder" : -23,
-			"bVisible" : true
-		},
-		"analysis_direction" : {
-			"bVisible" : false,
-			"iOrder" : -18
-		},
-		"signal_direction" : {
-			"sTitle" : "Signal direction",
-			"bVisible" : false,
-			"iOrder" : -17
-		},
-		"key_positive_control" : {
-			"bVisible" : false,
-			"iOrder" : -16
-		},
-		"assay_function_type" : {
-			"bVisible" : false,
-			"iOrder" : -15
-		},
-		"intended_target_type" : {
-			"sTitle" : "Target Type",
-			"bVisible" : false,
-			"iOrder" : -14
-		},
-		"intended_target_type_sub" : {
-			"bVisible" : false,
-			"iOrder" : -13
-		},
-		"intended_target_family" : {
-			"sTitle" : "Target Family",
-			"bVisible" : false,
-			"iOrder" : -12
-		},
-		"intended_target_family_sub" : {
-			"bVisible" : false,
-			"iOrder" : -11
-		},
-		"intended_target_gene_symbol" : {
-			"sTitle" : "Intended target (gene)",
-			"bVisible" : true,
-			"iOrder" : -19
-		}		
-	}	
-}
-
-
 var config_study = {
 	"columns" : {
 		"_" : {
@@ -1181,284 +990,37 @@ var config_study = {
 			}
 		},
 
-		"TO_SENSITIZATION_INVITRO_SECTION" : {
-			"parameters" : {
-				"type_of_study" : {
-					"iOrder" : -11,
-					"sTitle" : "Study type",
-					"bVisible" : false
-				},
-				"type_of_method" : {
-					"iOrder" : -10,
-					"sTitle" : "Method type"
-				},
-				"data_gathering_instruments" : {
-					"iOrder" : -9,
-					"bVisible" : false
-				}
-			},
-			"protocol" : {
-				"guideline" : {
-					"iOrder" : -2,
-					"inMatrix" : true
-				},
-				"citation" : {
-					"bVisible" : true
-				}
-			},
-			"conditions" : {
-				"target gene" : {
-					"iOrder" : -7,
-					"inMatrix" : true
-				},
-				"cell line" : {
-					"iOrder" : -8
-				}
-			},
-			"effects" : {
-				"endpoint" : {
-					"bVisible" : true,
-					"iOrder" : -6,
-					"inMatrix" : true
-
-				},
-				"result" : {
-					"bVisible" : true,
-					"iOrder" : -5,
-					"inMatrix" : true
-				}
-			},
-			"interpretation" : {
-				"result" : {
-					"sTitle" : "Pos/Neg",
-					"iOrder" : -4,
-					"inMatrix" : true
-				},
-				"criteria" : {
-					"iOrder" : -3,
-					"bVisible" : false
-				}
-			}
-		},
-		"TO_SENSITIZATION_INCHEMICO_SECTION" : {
-			"parameters" : {
-				"type_of_study" : {
-					"iOrder" : -11,
-					"sTitle" : "Study type",
-					"bVisible" : false
-				},
-				"type_of_method" : {
-					"iOrder" : -10,
-					"sTitle" : "Method type"
-				},
-				"data_gathering_instruments" : {
-					"iOrder" : -9,
-					"bVisible" : false
-				}
-			},
-			"protocol" : {
-				"guideline" : {
-					"iOrder" : -2,
-					"inMatrix" : true
-				},
-				"citation" : {
-					"bVisible" : true
-				}
-			},
-			"conditions" : {
-				"protein" : {
-					"iOrder" : -7,
-					"sTitle" : "",
-					"inMatrix" : true
-				}
-			},
-			"effects" : {
-				"endpoint" : {
-					"bVisible" : true,
-					"inMatrix" : true,
-					"iOrder" : -6
-				},
-				"result" : {
-					"bVisible" : true,
-					"inMatrix" : true,
-					"iOrder" : -5
-				}
-			},
-			"interpretation" : {
-				"result" : {
-					"sTitle" : "Pos/Neg",
-					"inMatrix" : true,
-					"iOrder" : -4
-				},
-				"criteria" : {
-					"iOrder" : -3,
-					"sTitle" : "Potency",
-					"bVisible" : true
-				}
-			}
-		},
-		"TO_SENSITIZATION_LLNA_SECTION" : {
-
-			"parameters" : {
-				"p_vehicle" : {
-					"iOrder" : -4,
-					"sTitle" : "Vehicle",
-					"bVisible" : true,
-					"inMatrix" : false
-				},
-				"p_max dose tested" : {
-					"iOrder" : -3,
-					"sTitle" : "Max Dose Tested",
-					"bVisible" : true,
-					"inMatrix" : false
-				},
-				"type of study" : {
-					"iOrder" : -9,
-					"sTitle" : "Type of study",
-					"bVisible" : false,
-					"inMatrix" : false
-				}
-			},
-			"protocol" : {
-				"guideline" : {
-					"iOrder" : -9,
-					"sTitle" : "Type of study",
-					"inMatrix" : false
-				},
-				"uuid" : {
-					"bVisible" : false
-				},
-				"owner" : {
-					"bVisible" : false
-				},
-				"citation" : {
-					"bVisible" : true,
-					"sTitle" : "Reference",
-					"mRender" : function(data, type, full) {
-						return data["title"];
-					},
-					"iOrder" : -1,
-					"inMatrix" : true
-				}
-			},
-			"conditions" : {
-				"vehicle" : {
-					"iOrder" : -4,
-					"sTitle" : "Vehicle",
-					"inMatrix" : false,
-					"bVisible" : false
-				},
-				"max dose tested" : {
-					"iOrder" : -3,
-					"sTitle" : "Max Dose Tested",
-					"bVisible" : false,
-					"inMatrix" : true
-				}
-			},
-			"effects" : {
-				"endpoint" : {
-					"bVisible" : true,
-					"inMatrix" : true,
-					"iOrder" : -8
-				},
-
-				"result" : {
-					"bVisible" : true,
-					"inMatrix" : true,
-					"iOrder" : -7
-				},
-				"text" : {
-					"bVisible" : true,
-					"sTitle" : "",
-					"iOrder" : -6
-				}
-			},
-			"interpretation" : {
-				"result" : {
-					"sTitle" : "Pot. Cat.",
-					"inMatrix" : false,
-					"bVisible" : true,
-					"iOrder" : -5
-				},
-				"criteria" : {
-					"iOrder" : -3,
-					"bVisible" : false
-				}
-			}
-		},
-		"TO_SENSITIZATION_HUMANDB_SECTION" : {
-
-			"parameters" : {
-				"species" : {
-					"iOrder" : -9,
-					"sTitle" : "Species",
-					"bVisible" : true,
-					"inMatrix" : true
-				},
-				"type of study" : {
-					"iOrder" : -9,
-					"sTitle" : "Type of study",
-					"bVisible" : false,
-					"inMatrix" : false
-				}
-			},
-			"protocol" : {
-				"guideline" : {
-					"iOrder" : -9,
-					"sTitle" : "Guideline",
-					"inMatrix" : false,
-					"bVisible" : false
-				},
-				"uuid" : {
-					"bVisible" : false
-				},
-				"owner" : {
-					"bVisible" : false
-				},
-				"citation" : {
-					"bVisible" : true,
-					"sTitle" : "Reference",
-					"mRender" : function(data, type, full) {
-						return data["title"];
-					},
-					"iOrder" : -1,
-					"inMatrix" : true
-				}
-			},
-			"conditions" : {
-
-			},
-			"effects" : {
-				"endpoint" : {
-					"bVisible" : true,
-					"inMatrix" : true,
-					"iOrder" : -8
-				},
-
-				"result" : {
-					"bVisible" : true,
-					"inMatrix" : true,
-					"iOrder" : -7
-				},
-				"text" : {
-					"bVisible" : true,
-					"sTitle" : "",
-					"iOrder" : -6
-				}
-			},
-			"interpretation" : {
-				"result" : {
-					"sTitle" : "Interpretation",
-					"inMatrix" : false,
-					"bVisible" : true,
-					"iOrder" : -5
-				},
-				"criteria" : {
-					"iOrder" : -3,
-					"bVisible" : false
-				}
-			}
-		},
+        "TO_SENSITIZATION_INVITRO_SECTION": {
+			"parameters" : config_ce0["parameters"],
+			"effects" : config_ce0["effects"],
+			"conditions" : config_ce0["conditions"],			
+			"protocol" : config_ce0["protocol"],
+			"interpretation" : config_ce0["interpretation"]	
+        
+        },        
+        "TO_SENSITIZATION_INCHEMICO_SECTION": {
+			"parameters" : config_ce["parameters"],
+			"effects" : config_ce["effects"],
+			"conditions" : config_ce["conditions"],			
+			"protocol" : config_ce["protocol"],
+			"interpretation" : config_ce["interpretation"]	       
+        },                
+        "TO_SENSITIZATION_LLNA_SECTION" : {
+			"parameters" : config_ce1["parameters"],
+			"effects" : config_ce1["effects"],
+			"conditions" : config_ce1["conditions"],			
+			"protocol" : config_ce1["protocol"],
+			"interpretation" : config_ce1["interpretation"]	
+        },
+        "TO_SENSITIZATION_HUMANDB_SECTION": {
+			"parameters" : config_ce2["parameters"],
+			"effects" : config_ce2["effects"],
+			"conditions" : config_ce2["conditions"],			
+			"protocol" : config_ce2["protocol"],
+			"interpretation" : config_ce2["interpretation"]	
+      
+        },     
+        
 		"TO_PHOTOTRANS_AIR_SECTION" : {
 			"parameters" : {
 				"reactant" : {
@@ -2558,20 +2120,21 @@ var config_study = {
 				"distribution_type" : {
 					"sTitle" : "Distribution type",
 					"iOrder" : -23
-				},
-				"testmat_form" : {
-					"sTitle" : "Test Material Form",
-					"bVisible" : true,
-					"iOrder" : -24
-				}
-			},
-			"conditions" : {
-				"seq_num" : {
-					"sTitle" : "Passage num.",
-					"iOrder" : -22
-				},
-				"medium" : {
-					"sTitle" : "Medium",
+                },
+                "testmat_form": {
+                    "sTitle": "Test Material Form",
+                    "bVisible": false,
+                    "iOrder": -24
+                }
+            },
+            "conditions": {
+                "seq_num": {
+                    "sTitle": "Passage num.",
+                    "iOrder": -22,
+                    "bVisible": false
+                },
+                "medium": {
+                    "sTitle": "Medium",
 					"iOrder" : -14,
 					"bVisible" : true,
 					"inMatrix" : true
@@ -2583,13 +2146,16 @@ var config_study = {
 				},
 				"n" : {
 					"iOrder" : -16
-				},
-				"phraseother_percentile" : {
-					"bVisible" : false
-				}
-			},
-			"effects" : {
-				"endpoint" : {
+                },                
+                "phraseother_percentile" : {
+                	"bVisible": false
+                },
+                "std_dev" : {
+                	"bVisible": false
+                }                
+            },
+            "effects": {
+                "endpoint": {
 					"iOrder" : -20,
 					"bVisible" : true,
 					"inMatrix" : true
@@ -2839,9 +2405,13 @@ var config_study = {
 				},
 				"criteria" : {
 					"bVisible" : false
-				}
-			}
-		},
+                    }
+                },
+                "uuid" : {
+               	 "bVisible": false
+                }
+                
+            },            
 		"SPECIFIC_SURFACE_AREA_SECTION" : {
 			"parameters" : {
 				"type of method" : {
