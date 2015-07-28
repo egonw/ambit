a86293eccc788ad83799f23a69095d43f5225970
ntk73 <ntk73@a469c33f-3a41-0410-815f-a20f7f95d01d>
2015-07-28 07:44:35 +0000

    Added new JSON parsing utils.
    git-svn-id: svn://svn.code.sf.net/p/ambit/code/branches/ambit-3.0.0-SNAPSHOT@6714 a469c33f-3a41-0410-815f-a20f7f95d01d

--- ambit2-rules/src/main/java/ambit2/rules/json/JSONParsingUtils.java
+++ ambit2-rules/src/main/java/ambit2/rules/json/JSONParsingUtils.java
@@ -1,11 +1,19 @@
 package ambit2.rules.json;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.codehaus.jackson.JsonNode;
 
+import ambit2.base.data.StructureRecord;
+
 public class JSONParsingUtils 
 {
+	public static enum STRUCTURE_RECORD_INPUT_INFO {
+		all, smiles, inchi
+	}
+	
 	
 	public static void jsonNodeToJavaField(JsonNode node, Object targetObj, String fieldName, boolean FlagExactFieldTypeMatch) throws Exception
 	{
@@ -68,7 +76,7 @@ public class JSONParsingUtils
 	 * @param isRequired
 	 * @return String value extracted from the JSON node or empty string if isRecquired = false (this case 
 	 * is not treated as an error hence no exception is thrown)
-	 * @throws Exception with the JSON error or missing value error (if isRecquired = true) 
+	 * @throws Exception with the JSON error or missing value error (if isRequired = true) 
 	 */
 	public static String extractStringKeyword(JsonNode node, String keyword, boolean isRequired) throws Exception
 	{
@@ -99,7 +107,7 @@ public class JSONParsingUtils
 	 * @param isRequired
 	 * @return Double value extracted from the JSON node or null pointer if isRecquired = false (this case 
 	 * is not treated as an error hence no exception is thrown)
-	 * @throws Exception with the JSON error or missing value error (if isRecquired = true)
+	 * @throws Exception with the JSON error or missing value error (if isRequired = true)
 	 */
 	public static Double extractDoubleKeyword(JsonNode node, String keyword, boolean isRequired) throws Exception
 	{
@@ -129,7 +137,7 @@ public class JSONParsingUtils
 	 * @param isRequired
 	 * @return Integer value extracted from the JSON node or null pointer if isRecquired = false (this case 
 	 * is not treated as an error hence no exception is thrown)
-	 * @throws Exception with the JSON error or missing value error (if isRecquired = true)
+	 * @throws Exception with the JSON error or missing value error (if isRequired = true)
 	 */
 	public static Integer extractIntKeyword(JsonNode node, String keyword, boolean isRequired) throws Exception
 	{
@@ -153,7 +161,209 @@ public class JSONParsingUtils
 	}
 	
 	
+	/**
+	 * 
+	 * @param node
+	 * @param keyword
+	 * @param isRequired
+	 * @return Boolean value extracted from the JSON node or null pointer if isRecquired = false (this case 
+	 * is not treated as an error hence no exception is thrown)
+	 * @throws Exception with the JSON error or missing value error (if isRequired = true)
+	 */
+	public static Boolean extractBooleanKeyword(JsonNode node, String keyword, boolean isRequired) throws Exception
+	{	
+		JsonNode keyNode = node.path(keyword);
+		if(keyNode.isMissingNode())
+		{
+			if(isRequired)
+				throw (new Exception("Keyword " + keyword + " is missing!"));
+			
+			return null;
+		}
+		
+		if (keyNode.isBoolean())
+		{	
+			return keyNode.asBoolean();
+		}
+		else
+		{	
+			throw (new Exception("Keyword " + keyword + " is not of type Boolean!"));
+		}			
+	}
 	
+	public static Object[] extractArrayKeyword(JsonNode node, String keyword, boolean isRequired) throws Exception
+	{
+		JsonNode keyNode = node.path(keyword);
+		if(keyNode.isMissingNode())
+		{
+			if(isRequired)
+				throw (new Exception("Keyword " + keyword + " is missing!"));
+			
+			return null;
+		}
+		
+		if (keyNode.isArray())
+		{
+			int n = keyNode.size();
+			Object objects[] = new Object[n];
+			for (int i = 0; i < n; i++)
+				objects[i] =  extractObject(keyNode.get(i));
+			
+			return objects;
+		}
+		else
+		{
+			throw (new Exception("Keyword " + keyword + " is not an array!"));
+		}
+	} 
 	
+	/**
+	 * 
+	 * @param node
+	 * @return
+	 */
+	public static Object extractObject (JsonNode node)
+	{
+		if (node.isTextual())
+		{
+			String s = node.asText();
+			if (s != null)
+				return s;
+		}
+		
+		if (node.isInt())
+		{
+			int i = node.asInt();
+			return  new Integer(i);
+		}
+		
+		if (node.isDouble())
+		{
+			double d  = node.asDouble();
+			return new Double(d);
+		}
+		
+		//TODO - eventually add array object extraction
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public static String objectToJsonField(Object obj)
+	{
+		if (obj == null)
+			return null;
+		
+		if (obj instanceof String)
+			return ("\"" + obj.toString() + "\"");
+		
+		if (obj instanceof Integer)
+			return obj.toString();
+		
+		if (obj instanceof Double)
+			return obj.toString();
+		
+		if (obj instanceof double[])
+		{
+			return toJsonField((double[]) obj);
+		}
+			
+		//TODO handle some other cases as arrays etc.
+		
+		return null;
+	}
+	/**
+	 * 
+	 * @param d
+	 * @return
+	 */
+	public static String toJsonField(double d[])
+	{
+		if (d == null)
+			return null;
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("[");
+		for (int i = 0; i < d.length; i++)
+		{
+			sb.append(d[i]);
+			if (i < (d.length-1))
+				sb.append(",");
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+	
+	public static List<StructureRecord> getStructureRecords(JsonNode node, 
+												String keyword, 
+												STRUCTURE_RECORD_INPUT_INFO inputInfo, 
+												boolean isRequired) throws Exception
+	{
+		JsonNode keyNode = node.path(keyword);
+		if(keyNode.isMissingNode())
+		{
+			if(isRequired)
+				throw (new Exception("Keyword " + keyword + " is missing!"));
+			
+			return null;
+		}
+		
+		if (keyNode.isArray())
+		{
+			List<StructureRecord> records = new ArrayList<StructureRecord>();
+			int n = keyNode.size();
+			StringBuffer errors = new StringBuffer();
+			for (int i = 0; i < n; i++)
+			{	
+				try{
+					StructureRecord sr = getStructureRecord(keyNode.get(i), inputInfo);
+					records.add(sr);
+				}
+				catch(Exception e)
+				{
+					errors.append("Structure record error: " 
+							+ keyword + "[" + i+1+"] " + e.getMessage() + "\n");
+				}
+			}	
+			
+			if (!errors.toString().equals(""))
+				throw (new Exception(errors.toString()));
+			
+			return records;
+		}
+		else
+		{
+			throw (new Exception("Keyword " + keyword + " is not an array!"));
+		}
+		
+		
+	}
+	
+	public static StructureRecord getStructureRecord(JsonNode node, 
+			STRUCTURE_RECORD_INPUT_INFO inputInfo) throws Exception
+	{
+		switch (inputInfo)
+		{
+		case smiles:
+			
+			if (node.isTextual())
+			{
+				StructureRecord sr = new StructureRecord();
+				sr.setSmiles(node.asText());
+				return sr;
+			}
+			else
+				throw (new Exception("Json node is not textual!"));
+			
+		default:
+			break;
+		}
+		
+		return null;
+	}
 	
 }
