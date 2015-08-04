09afe61151f92733a542c2e6fa4ca791bec70b5c
vedina <vedina@a469c33f-3a41-0410-815f-a20f7f95d01d>
2015-08-04 13:23:31 +0000

    loggre options (opsin log level) and code formatting
    git-svn-id: svn://svn.code.sf.net/p/ambit/code/branches/ambit-3.0.0-SNAPSHOT@6756 a469c33f-3a41-0410-815f-a20f7f95d01d

--- ambit2-apps/ambit2-www/src/main/java/ambit2/rest/similarity/SimilarityResource.java
+++ ambit2-apps/ambit2-www/src/main/java/ambit2/rest/similarity/SimilarityResource.java
@@ -47,207 +47,231 @@ import ambit2.rest.query.StructureQueryResource;
  * @author nina
  * 
  */
-public class SimilarityResource<Q extends IQueryRetrieval<IStructureRecord>> extends StructureQueryResource<Q> {
+public class SimilarityResource<Q extends IQueryRetrieval<IStructureRecord>>
+		extends StructureQueryResource<Q> {
 
-    public final static String resource = "/similarity";
-    public final static String smiles = "smiles";
-    public final static String resourceID = String.format("/{%s}", smiles);
-    protected double threshold = 0.9;
-    protected String dataset_id;
-    protected boolean filterBySubstance = false;
-    protected IAtomContainer mol;
+	public final static String resource = "/similarity";
+	public final static String smiles = "smiles";
+	public final static String resourceID = String.format("/{%s}", smiles);
+	protected double threshold = 0.9;
+	protected String dataset_id;
+	protected boolean filterBySubstance = false;
+	protected IAtomContainer mol;
 
-    public SimilarityResource() {
-	super();
-    }
-
-    @Override
-    public String getCompoundInDatasetPrefix() {
-	if (dataset_prefixed_compound_uri)
-	    return dataset_id != null ? String.format("%s/%s", OpenTox.URI.dataset.getURI(), dataset_id) : "";
-
-	else
-	    return "";
-    }
-
-    protected String getDefaultTemplateURI(Context context, Request request, Response response) {
-	return (dataset_id == null) ? null : String.format("%s%s/%s%s", getRequest().getRootRef(),
-		OpenTox.URI.dataset.getURI(), dataset_id, PropertyResource.featuredef);
-
-	// String.format("riap://application/dataset/%s%s",dataset_id,PropertyResource.featuredef);
-    }
-
-    @Override
-    protected CSVReporter createCSVReporter() {
-	CSVReporter csvReporter = super.createCSVReporter();
-	csvReporter.setSimilarityColumn(Property.getInstance("metric",
-		queryObject == null ? "" : queryObject.toString(), "http://ambit.sourceforge.net"));
-	return csvReporter;
-    }
-
-    @Override
-    protected Q createQuery(Context context, Request request, Response response) throws ResourceException {
-	Form form = getResourceRef(getRequest()).getQueryAsForm();
-
-	try {
-	    Object bundleURI = OpenTox.params.bundle_uri.getFirstValue(form);
-	    Integer idbundle = bundleURI == null ? null : getIdBundle(bundleURI, request);
-	    SubstanceEndpointsBundle bundle = new SubstanceEndpointsBundle(idbundle);
-	    bundles = new SubstanceEndpointsBundle[1];
-	    bundles[0] = bundle;
-	} catch (Exception x) {
-	    bundles = null;
+	public SimilarityResource() {
+		super();
 	}
 
-	try {
-	    includeMol = "true".equals(form.getFirstValue("mol"));
-	} catch (Exception x) {
-	    includeMol = false;
-	}
-	folders = form.getValuesArray("folder");
-	
-	filterBySubstance = false;
-	try {
-	    String filter = form.getFirstValue("filterBySubstance");
-	    if (filter != null) {
-		filter = filter.toLowerCase();
-		filterBySubstance = "yes".equals(filter) || "on".equals(filter) ||  "true".equals(filter) ; 
-	    }
-	} catch (Exception x) {
-	    filterBySubstance = false;
-	}
-	
-	mol = getMolecule(form);
-	if ((mol == null) || (mol.getAtomCount() == 0))
-	    throw new ResourceException(Status.CLIENT_ERROR_NOT_FOUND, "Empty molecule");
-	threshold = 0.0;
-	try {
-	    threshold = new Double(Reference.decode(form.getFirstValue("threshold")));
-	} catch (Exception x) {
-	    threshold = 0.9;
+	@Override
+	public String getCompoundInDatasetPrefix() {
+		if (dataset_prefixed_compound_uri)
+			return dataset_id != null ? String.format("%s/%s",
+					OpenTox.URI.dataset.getURI(), dataset_id) : "";
+
+		else
+			return "";
 	}
 
-	QuerySimilarityBitset q = new QuerySimilarityBitset();
-	q.setChemicalsOnly(true);
-	q.setThreshold(threshold);
-	q.setCondition(NumberCondition.getInstance(">"));
-	q.setName("Similarity");
+	protected String getDefaultTemplateURI(Context context, Request request,
+			Response response) {
+		return (dataset_id == null) ? null : String.format("%s%s/%s%s",
+				getRequest().getRootRef(), OpenTox.URI.dataset.getURI(),
+				dataset_id, PropertyResource.featuredef);
 
-	try {
-	    q.setForceOrdering(((AmbitApplication) getApplication()).isSimilarityOrder());
-	} catch (Exception x) {
-	    q.setForceOrdering(true);
+		// String.format("riap://application/dataset/%s%s",dataset_id,PropertyResource.featuredef);
 	}
 
-	try {
-	    q.setValue(getBitset(mol));
-	    if (filterBySubstance) {
-		ChemicalBySubstanceRelation qa = new ChemicalBySubstanceRelation();
-		QueryCombinedStructure qc = new QueryCombinedStructure();
-		qc.add(q);
-		qc.setChemicalsOnly(true);
-		qc.setScope(qa);
-		setTemplate(createTemplate(context, request, response));
-		setGroupProperties(context, request, response);
-		return (Q) qc;		
-	    } else if ((folders != null) && (folders.length > 0)) {
-		ChemicalByQueryFolder qa = new ChemicalByQueryFolder(folders);
-		QueryCombinedStructure qc = new QueryCombinedStructure();
-		qc.add(q);
-		qc.setChemicalsOnly(true);
-		qc.setScope(qa);
-		setTemplate(createTemplate(context, request, response));
-		setGroupProperties(context, request, response);
-		return (Q) qc;
-	    } else {
-		Object datasetURI = OpenTox.params.dataset_uri.getFirstValue(form);
-		ISourceDataset srcdataset = null;
-		if (datasetURI != null)
-		    try {
-			srcdataset = getDataset(datasetURI.toString());
-			QueryCombinedStructure qc = new QueryCombinedStructure();
-			qc.add(q);
-			qc.setChemicalsOnly(true);
-			if (srcdataset instanceof SourceDataset) {
-			    ChemicalByDataset cd = new ChemicalByDataset(new Integer(srcdataset.getID()));
-			    qc.setScope(cd);
-			    setTemplate(createTemplate(context, request, response));
-			    setGroupProperties(context, request, response);
-			    return (Q) qc;
-			} else {
-			    // TODO, resort to all db
-			}
-		    } catch (Exception x) {
-			srcdataset = null;
-		    }
+	@Override
+	protected CSVReporter createCSVReporter() {
+		CSVReporter csvReporter = super.createCSVReporter();
+		csvReporter.setSimilarityColumn(Property.getInstance("metric",
+				queryObject == null ? "" : queryObject.toString(),
+				"http://ambit.sourceforge.net"));
+		return csvReporter;
+	}
 
-		QueryCombinedStructure qc = null;
+	@Override
+	protected Q createQuery(Context context, Request request, Response response)
+			throws ResourceException {
+		Form form = getResourceRef(getRequest()).getQueryAsForm();
+
 		try {
-
-		    this.dataset_id = Reference.decode(getRequest().getAttributes().get(DatasetResource.datasetKey)
-			    .toString());
-		    qc = new QueryCombinedStructure();
-		    qc.add(q);
-		    qc.setChemicalsOnly(true);
-		    ChemicalByDataset cd = new ChemicalByDataset(new Integer(dataset_id));
-		    qc.setScope(cd);
-		    setTemplate(createTemplate(context, request, response));
-		    setGroupProperties(context, request, response);
-		    return (Q) qc;
+			Object bundleURI = OpenTox.params.bundle_uri.getFirstValue(form);
+			Integer idbundle = bundleURI == null ? null : getIdBundle(
+					bundleURI, request);
+			SubstanceEndpointsBundle bundle = new SubstanceEndpointsBundle(
+					idbundle);
+			bundles = new SubstanceEndpointsBundle[1];
+			bundles[0] = bundle;
 		} catch (Exception x) {
-		    setTemplate(createTemplate(context, request, response));
-		    setGroupProperties(context, request, response);
-		    return (Q) q;
+			bundles = null;
 		}
-	    }
-	} catch (Exception x) {
-	    throw new ResourceException(Status.SERVER_ERROR_INTERNAL, x);
+
+		try {
+			includeMol = "true".equals(form.getFirstValue("mol"));
+		} catch (Exception x) {
+			includeMol = false;
+		}
+		folders = form.getValuesArray("folder");
+
+		filterBySubstance = false;
+		try {
+			String filter = form.getFirstValue("filterBySubstance");
+			if (filter != null) {
+				filter = filter.toLowerCase();
+				filterBySubstance = "yes".equals(filter) || "on".equals(filter)
+						|| "true".equals(filter);
+			}
+		} catch (Exception x) {
+			filterBySubstance = false;
+		}
+
+		mol = getMolecule(form);
+		if ((mol == null) || (mol.getAtomCount() == 0))
+			throw new ResourceException(Status.CLIENT_ERROR_NOT_FOUND,
+					"Empty molecule");
+		threshold = 0.0;
+		try {
+			threshold = new Double(Reference.decode(form
+					.getFirstValue("threshold")));
+		} catch (Exception x) {
+			threshold = 0.9;
+		}
+
+		QuerySimilarityBitset q = new QuerySimilarityBitset();
+		q.setChemicalsOnly(true);
+		q.setThreshold(threshold);
+		q.setCondition(NumberCondition.getInstance(">"));
+		q.setName("Similarity");
+
+		try {
+			q.setForceOrdering(((AmbitApplication) getApplication())
+					.isSimilarityOrder());
+		} catch (Exception x) {
+			q.setForceOrdering(true);
+		}
+
+		try {
+			q.setValue(getBitset(mol));
+			if (filterBySubstance) {
+				ChemicalBySubstanceRelation qa = new ChemicalBySubstanceRelation();
+				QueryCombinedStructure qc = new QueryCombinedStructure();
+				qc.add(q);
+				qc.setChemicalsOnly(true);
+				qc.setScope(qa);
+				setTemplate(createTemplate(context, request, response));
+				setGroupProperties(context, request, response);
+				return (Q) qc;
+			} else if ((folders != null) && (folders.length > 0)) {
+				ChemicalByQueryFolder qa = new ChemicalByQueryFolder(folders);
+				QueryCombinedStructure qc = new QueryCombinedStructure();
+				qc.add(q);
+				qc.setChemicalsOnly(true);
+				qc.setScope(qa);
+				setTemplate(createTemplate(context, request, response));
+				setGroupProperties(context, request, response);
+				return (Q) qc;
+			} else {
+				Object datasetURI = OpenTox.params.dataset_uri
+						.getFirstValue(form);
+				ISourceDataset srcdataset = null;
+				if (datasetURI != null)
+					try {
+						srcdataset = getDataset(datasetURI.toString());
+						QueryCombinedStructure qc = new QueryCombinedStructure();
+						qc.add(q);
+						qc.setChemicalsOnly(true);
+						if (srcdataset instanceof SourceDataset) {
+							ChemicalByDataset cd = new ChemicalByDataset(
+									new Integer(srcdataset.getID()));
+							qc.setScope(cd);
+							setTemplate(createTemplate(context, request,
+									response));
+							setGroupProperties(context, request, response);
+							return (Q) qc;
+						} else {
+							// TODO, resort to all db
+						}
+					} catch (Exception x) {
+						srcdataset = null;
+					}
+
+				QueryCombinedStructure qc = null;
+				try {
+
+					this.dataset_id = Reference.decode(getRequest()
+							.getAttributes().get(DatasetResource.datasetKey)
+							.toString());
+					qc = new QueryCombinedStructure();
+					qc.add(q);
+					qc.setChemicalsOnly(true);
+					ChemicalByDataset cd = new ChemicalByDataset(new Integer(
+							dataset_id));
+					qc.setScope(cd);
+					setTemplate(createTemplate(context, request, response));
+					setGroupProperties(context, request, response);
+					return (Q) qc;
+				} catch (Exception x) {
+					setTemplate(createTemplate(context, request, response));
+					setGroupProperties(context, request, response);
+					return (Q) q;
+				}
+			}
+		} catch (Exception x) {
+			throw new ResourceException(Status.SERVER_ERROR_INTERNAL, x);
+		}
+
 	}
 
-    }
-
-    public BitSet getBitset(IAtomContainer molecule) throws AmbitException {
-	FingerprintGenerator gen = new FingerprintGenerator(new Fingerprinter());
-	return gen.process(molecule);
-    }
-
-    protected ISourceDataset getDataset(String uri) throws InvalidResourceIDException {
-	Map<String, Object> vars = new HashMap<String, Object>();
-	Template template = OpenTox.URI.dataset.getTemplate(getRequest().getRootRef());
-	String id = null;
-	try {
-	    template.parse(uri, vars);
-	    id = vars.get(OpenTox.URI.dataset.getKey()).toString();
-	} catch (Exception x) {
-	    return null;
+	public BitSet getBitset(IAtomContainer molecule) throws AmbitException {
+		FingerprintGenerator gen = new FingerprintGenerator(new Fingerprinter());
+		return gen.process(molecule);
 	}
 
-	if (id != null)
-	    try {
-		Integer idnum = new Integer(Reference.decode(id.toString()));
-		SourceDataset dataset = new SourceDataset();
-		dataset.setID(idnum);
-		return dataset;
-	    } catch (NumberFormatException x) {
-		if (id.toString().startsWith(DatasetStructuresResource.QR_PREFIX)) {
-		    String key = id.toString().substring(DatasetStructuresResource.QR_PREFIX.length());
-		    try {
-			ISourceDataset dataset = new StoredQuery();
-			dataset.setID(Integer.parseInt(key.toString()));
-			return dataset;
-		    } catch (NumberFormatException xx) {
-			throw new InvalidResourceIDException(id);
-		    }
+	protected ISourceDataset getDataset(String uri)
+			throws InvalidResourceIDException {
+		Map<String, Object> vars = new HashMap<String, Object>();
+		Template template = OpenTox.URI.dataset.getTemplate(getRequest()
+				.getRootRef());
+		String id = null;
+		try {
+			template.parse(uri, vars);
+			id = vars.get(OpenTox.URI.dataset.getKey()).toString();
+		} catch (Exception x) {
+			return null;
 		}
-	    } catch (Exception x) {
-		throw new InvalidResourceIDException(id);
-	    }
-	return null;
-    }
 
-    @Override
-    protected String[] getDefaultGroupProperties() {
-	return new String[] { Property.opentox_CAS, Property.opentox_EC, Property.opentox_Name,
-		Property.opentox_TradeName, Property.opentox_IupacName, Property.opentox_IUCLID5_UUID , Property.opentox_SMILES, Property.opentox_InChI_std, Property.opentox_InChIKey_std};
-    }
+		if (id != null)
+			try {
+				Integer idnum = new Integer(Reference.decode(id.toString()));
+				SourceDataset dataset = new SourceDataset();
+				dataset.setID(idnum);
+				return dataset;
+			} catch (NumberFormatException x) {
+				if (id.toString().startsWith(
+						DatasetStructuresResource.QR_PREFIX)) {
+					String key = id.toString().substring(
+							DatasetStructuresResource.QR_PREFIX.length());
+					try {
+						ISourceDataset dataset = new StoredQuery();
+						dataset.setID(Integer.parseInt(key.toString()));
+						return dataset;
+					} catch (NumberFormatException xx) {
+						throw new InvalidResourceIDException(id);
+					}
+				}
+			} catch (Exception x) {
+				throw new InvalidResourceIDException(id);
+			}
+		return null;
+	}
+
+	@Override
+	protected String[] getDefaultGroupProperties() {
+		return new String[] { Property.opentox_CAS, Property.opentox_EC,
+				Property.opentox_Name, Property.opentox_TradeName,
+				Property.opentox_IupacName, Property.opentox_IUCLID5_UUID,
+				Property.opentox_SMILES, Property.opentox_InChI_std,
+				Property.opentox_InChIKey_std };
+	}
 }
