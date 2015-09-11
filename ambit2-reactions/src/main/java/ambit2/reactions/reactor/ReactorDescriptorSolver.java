4acfef6b5d3ec66052962b64ab755e0c49e63c7d
ntk73 <ntk73@a469c33f-3a41-0410-815f-a20f7f95d01d>
2015-09-11 14:45:02 +0000

    Added two new classes: ReactorInfoPack and ReactorDescriptorSolver.
    git-svn-id: svn://svn.code.sf.net/p/ambit/code/branches/ambit-3.0.0-SNAPSHOT@6819 a469c33f-3a41-0410-815f-a20f7f95d01d

---
+++ ambit2-reactions/src/main/java/ambit2/reactions/reactor/ReactorDescriptorSolver.java
@@ -0,0 +1,28 @@
+package ambit2.reactions.reactor;
+
+import ambit2.rules.conditions.AbstractDescriptorSolver;
+
+public class ReactorDescriptorSolver extends AbstractDescriptorSolver
+{
+	protected ReactorInfoPack reactInfo = null;
+	
+	public ReactorDescriptorSolver()
+	{
+		setup();
+	}
+	
+	protected void setup()
+	{
+		//TODO
+	}
+		
+	@Override
+	public Object calculateDescriptor(String descrName, Object target) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
---
+++ ambit2-reactions/src/main/java/ambit2/reactions/reactor/ReactorInfoPack.java
@@ -0,0 +1,7 @@
+package ambit2.reactions.reactor;
+
+public class ReactorInfoPack 
+{
+	public Reactor reactor = null;
+	public ReactorNode node = null;
+}
